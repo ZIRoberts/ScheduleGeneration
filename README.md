2dc05e834856605fc5b6e3effe535116b0b24f16
Theodread <49044136+Theodread@users.noreply.github.com>
2020-10-11 13:08:45 -0400

    Added Example File, and updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # ScheduleGeneration
-Terminal Base Schedule Generation Program
+Terminal base schedule generation program made with python.
+
+Motive:
+This program is made with the intention of being presented to
+York College of Pennsylvania. The driving motion be hind this project is so then
+students will no longer have to spend time creating their own schedules or
+worrying whether or not they chose their best schedule. By running the example
+file you will see that I had 36 possible schedules for one of my semesters.
+I only spent the time to make 4 schedules and to compare them.
+
+How to use:
+Navigate to the generation.py file and open it. On line 8 you input the name of
+the csv (comma separated variable) file you whish to use as an input. This csv
+file must be in the same folder and match the same layout as the "exmaple.csv".
+
+NOTE: The csv must be encoded in UTF-8
+
+To run the program you must navigate to its file directory via Terminal and
+then type 'python.exe generation.py' and the output will be printed to the
+Terminal
+
+Future Plans/stages of progression:
+1. Refactor required layout for input csv
+2. Print output schedules as csv files
+3. Make GUI and have output displayed graphically
+4. Have it scrape the YCP schedule of classes page to get all class information
+   instead of requiring an input csv file
+5. Make it so you can sort your course how ever want, what time you want to
+   have a specific course, what professor you want, etc.
---
+++ generation.py
@@ -0,0 +1,133 @@
+import pandas as pd 
+import numpy as np
+
+##########################################################################################################################################################################
+# CHANGE FILE NAME IN read_scv TO CHANGE THE FILE THAT IS READ INTO THE PROGRAM
+##########################################################################################################################################################################
+classOptions = pd.DataFrame()
+classOptions = pd.read_csv("Example.csv")
+classOptions = classOptions.fillna(-1)
+rows = classOptions.shape[0]
+stepIteration = []
+sceduleCount = 1
+
+#remove any iterations with over laps
+def chkIteration(myList):
+    global stepIteration
+    schedule = pd.DataFrame()
+    for c in range(len(stepIteration)):
+        classStep = stepIteration[c]
+        if c == 0:
+            schedule = myList[c].iloc[classStep * 2: (classStep * 2) + 2]
+        else:
+            schedule = schedule.append(myList[c].iloc[classStep * 2: (classStep * 2) + 2])
+            
+        schedule.index = range(schedule.shape[0])
+    validate(schedule)
+
+def validate(mySchedule):
+    days = ['M', 'T', 'W', 'R', 'F']
+    global sceduleCount
+    for c in range(len(days)):
+        #CED stands for Classes Each Day
+        CED = mySchedule[[days[c]]]
+
+        labDay = mySchedule[["Lab_Day"]]
+        labTime = mySchedule[["Lab_Time"]]
+        for i in range (0, labDay.shape[0]):
+            test = labDay.iloc[i, 0]
+            if test != -1:
+                if (str(test) == days[c]):
+                    #Lab must be defined in this mannor or it will not correctly be appened onto CED
+                    lab = [{days[c] : labTime.iloc[i, 0]}, {days[c] : labTime.iloc[i + 1, 0]}]
+                    CED = CED.append(lab, ignore_index=True)
+
+        #Compare start and end times of classes to see if any over laps. disregards inputs of -1 which represents NaN         
+        valid = True;
+        for i in range (0, int((CED.shape[0])/2)):
+            startTime = CED.iloc[i * 2, 0]
+            endTime = CED.iloc[i * 2 + 1, 0]
+            if startTime != -1 or endTime != -1:   
+                for j in range (0, int((CED.shape[0])/2)):
+                    #o means other; other class. oStartTime = other class start time 
+                    oStartTime = CED.iloc[j * 2, 0]
+                    oEndTime = CED.iloc[j * 2 + 1, 0]
+                    if j != i and oStartTime != -1 and oEndTime != -1:
+                        chk1 = False
+                        chk2 = False
+                        
+                        #Checks to see if the initial class is before the other class
+                        if startTime < oStartTime and startTime < oEndTime and endTime < oEndTime and endTime < oStartTime:
+                            chk1 = True
+                        #Checks to see if the inital class is after the other class
+                        if startTime > oStartTime and startTime > oEndTime and endTime > oEndTime and endTime > oStartTime:
+                            chk2 = True
+                 
+                        if chk1 == False and chk2 == False:
+                            valid = False
+
+                    if valid == False:
+                        break
+            if valid == False:
+                break
+        if valid == False:
+            break
+        
+    if valid:
+        #prints scheduel number, valid schedule, converts float64 to int64 and adds a divider for readability 
+        titles = mySchedule.columns
+        for p in range (0, len(titles)):
+            if  mySchedule[titles[p]].dtype == np.float64:
+                mySchedule[titles[p]] = mySchedule[titles[p]].astype(np.int64)
+
+        print("##############################################################################################################################################################")
+        print("Schedule #" + str(sceduleCount))
+        print(mySchedule)
+        sceduleCount += 1
+
+                
+
+#generations all possible iterations of classes 
+def class_recursion(mylist, currentStep):
+    #mylist is a list of dataframes
+    global stepIteration
+
+    #.shape[0] returns the number of rows
+    stepLength = mylist[currentStep].shape[0] / 2
+    for c in range(0, int(stepLength)):
+        if currentStep != (len(mylist) - 1):   
+            class_recursion(mylist, currentStep + 1)
+        else:
+            chkIteration(mylist)
+            
+        stepIteration[currentStep] += 1
+        if stepIteration[currentStep] == stepLength:
+            stepIteration[currentStep] = 0
+
+
+#Identifies the rows in which a new class is begins
+className = []
+classNum = 0   
+for c in range(rows):
+    if c % 2 == 0:
+        if classOptions.iloc[c, 0] == 1:
+            className.append(classOptions.iloc[c, 2])
+            classNum += 1
+
+#Sorts each class into their own datafarm for that particular class then
+#add them to a list of dataframes
+classSorted = []
+for c in range(classNum):
+    df = classOptions.loc[classOptions['Class'] == className[c]]
+    classSorted.append(df)
+
+#fixes the index numbers within the df for the sorted classes
+generation = []
+for c in range (len(classSorted)):
+    classSorted[c].index = range(classSorted[c].shape[0])
+    stepIteration.append(0)
+    df = classSorted[c].loc[[0,1],:]
+    generation.append(df)
+
+step = 0
+class_recursion(classSorted, step)
